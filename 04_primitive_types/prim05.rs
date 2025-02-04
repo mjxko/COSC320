3b6468cd57c0599c2f5fbe1443f36f1b098c6af2
mjxko <mahrajalmazrouei@gmail.com>
2025-02-04 10:03:58 +0400

    Add files via upload

---
+++ 04_primitive_types/prim05.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+
+    // Destructuring the tuple into `name` and `age`
+    let (name, age) = cat;
+
+    println!("{name} is {age} years old");
+}
+
