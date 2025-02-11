cdbdba1296b5d0af2b5b8a62d15dafd435ea7cb4
mjxko <mahrajalmazrouei@gmail.com>
2025-02-11 09:30:22 +0400

    Add files via upload

---
+++ 02_functions/func01.rs
@@ -0,0 +1,12 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+
+fn call_me() {
+    println!("call meeee <3");
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
+
+
