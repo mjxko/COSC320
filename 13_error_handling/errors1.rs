5b159623d1fdb5043c2ffabf60f8f9d135d45786
mjxko <mahrajalmazrouei@gmail.com>
2025-02-25 10:09:48 +0400

    Add files via upload

---
+++ 13_error_handling/errors1.rs
@@ -0,0 +1,32 @@
+fn generate_nametag_text(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("Empty names aren't allowed".to_string())
+    } else {
+        Ok(format!("Hi! My name is {name}"))
+    }
+}
+fn main() {
+    // You can optionally experiment here.
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates_nametag_text_for_a_nonempty_name() {
+        assert_eq!(
+            generate_nametag_text("Beyoncé".to_string()),
+            Ok("Hi! My name is Beyoncé".to_string())
+        );
+    }
+
+    #[test]
+    fn explains_why_generating_nametag_text_fails() {
+        assert_eq!(
+            generate_nametag_text(String::new()),
+            Err("Empty names aren't allowed".to_string())
+        );
+    }
+}
---
+++ 13_error_handling/errors6.rs
@@ -0,0 +1,88 @@
+use std::num::ParseIntError;
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+#[derive(PartialEq, Debug)]
+enum ParsePosNonzeroError {
+    Creation(CreationError),
+    ParseInt(ParseIntError),
+}
+
+
+impl ParsePosNonzeroError {
+    fn from_creation(err: CreationError) -> Self {
+        Self::Creation(err)
+    }
+}
+
+
+impl From<ParseIntError> for ParsePosNonzeroError {
+    fn from(error: ParseIntError) -> Self {Self::ParseInt(error)}
+}
+
+
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<Self, CreationError> {
+        match value {
+            x if x < 0 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            x => Ok(Self(x as u64)),
+        }
+    }
+
+    fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
+        let parsed_value = s.parse::<i64>().map_err(ParsePosNonzeroError::from);
+        match parsed_value {
+            Ok(x) => Self::new(x).map_err(ParsePosNonzeroError::from_creation),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_error() {
+        assert!(matches!(
+            PositiveNonzeroInteger::parse("not a number"),
+            Err(ParsePosNonzeroError::ParseInt(_)),
+        ));
+    }
+
+    #[test]
+    fn test_negative() {
+        assert_eq!(
+            PositiveNonzeroInteger::parse("-555"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Negative)),
+        );
+    }
+
+    #[test]
+    fn test_zero() {
+        assert_eq!(
+            PositiveNonzeroInteger::parse("0"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Zero)),
+        );
+    }
+
+    #[test]
+    fn test_positive() {
+        let x = PositiveNonzeroInteger::new(42).unwrap();
+        assert_eq!(x.0, 42);
+        assert_eq!(PositiveNonzeroInteger::parse("42"), Ok(x));
+    }
+}
